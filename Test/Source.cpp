8dc6c753105546be7a8d52e9aa6b91655aee5eb0
tofb15 <fas_th@hotmail.com>
2019-02-15 12:37:49 +0100

    Added more functions

--- Test/Source.cpp
+++ Test/Source.cpp
@@ -2,14 +2,14 @@
 
 class Texture {
 public:
-	virtual bool LoadFromFile(const char*)						= 0;
+	virtual bool LoadFromFile(const char*)							= 0;
 private:
 	Texture();
 };
 
 class Material {
 public:
-	virtual bool LoadFromFile(const char*)						= 0;
+	virtual bool LoadFromFile(const char*)							= 0;
 	virtual void AddTexture(Texture*);
 private:
 	std::vector<Texture*> textures;
@@ -29,7 +29,7 @@ private:
 
 class Mesh {
 public:
-	virtual bool LoadFromFile(const char*) = 0;
+	virtual bool LoadFromFile(const char*)							= 0;
 	virtual void SetTechnique(Technique*);
 private:
 	std::vector<VertexBuffer*> vertexBuffers;
@@ -38,12 +38,19 @@ private:
 
 class Renderer {
 public:
-	static Renderer* MakeRenderer();
-	virtual Texture* MakeTexture()								= 0;
-	virtual Mesh* MakeMesh()									= 0;
-	virtual Material* MakeMaterial()							= 0;
-	virtual RenderState* MakeRenderState()						= 0;
-	virtual Technique* MakeTechnique(Material*, RenderState*)	= 0;
+	static	Renderer*		MakeRenderer();
+	virtual Texture*		MakeTexture()							= 0;
+	virtual Mesh*			MakeMesh()								= 0;
+	virtual Material*		MakeMaterial()							= 0;
+	virtual RenderState*	MakeRenderState()						= 0;
+	virtual Technique*		MakeTechnique(Material*, RenderState*)	= 0;
+
+
+	virtual void			Submit(Mesh*)							= 0; //How will this work with multi-threaded submitions? Should we submit an "Entity"-class insteed of a "Mesh"-class?
+	virtual void			ClearSubmitions()						= 0; //Should we have this?
+	virtual void			Frame()									= 0; //How will this work with multi-threading? One thread to rule them all?
+	virtual void			Present()								= 0; //How will this work with multi-threading? One thread to rule them all?
+	virtual void			ClearFrame()							= 0; //How will this work with multi-threading?
 };
 
 
@@ -66,10 +73,10 @@ int main() {
 	std::vector<Technique*> techniques;
 	std::vector<RenderState*> renderStates;
 
-#pragma region CreateUniqeMesh
+#pragma region CreateUniqueMesh
 	//Load meshes and materials from file
 	Mesh* mesh = renderer->MakeMesh();
-	mesh->LoadFromFile(".obj");
+	mesh->LoadFromFile(".obj"); //Vertexbuffer loaded here but should be able to be added seperatly aswell. Should we load material and texture here aswell?
 	meshes.push_back(mesh);
 
 	//Create a material
@@ -95,6 +102,18 @@ int main() {
 
 	//Attach Technique to mesh
 	mesh->SetTechnique(tech);
+#pragma endregion
+
+#pragma region Render
+	for (size_t i = 0; i < meshes.size(); i++)
+	{
+		//Submit all meshes that should be rendered.
+		renderer->Submit(meshes[i]);
+	}
+
+	renderer->Frame();	//Draw all meshes in the submit list. Do we want to support multiple frames? What if we want to render split-screen? Could differend threads prepare different frames?
+	renderer->Present();//Present frame to screen
+
 #pragma endregion
 
 	return 0;
