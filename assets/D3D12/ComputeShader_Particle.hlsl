cd38c7dab230f949d6c1ba8658a8ba3e6b7ade8c
tofb15 <fas_th@hotmail.com>
2019-03-21 11:16:17 +0100

    no message

--- assets/D3D12/ComputeShader_Particle.hlsl
+++ assets/D3D12/ComputeShader_Particle.hlsl
@@ -15,7 +15,7 @@ uint arrayPos = (DTid.x * 3);
 //particles[arrayPos + 1] = cos(DTid.x);
 
 particles[arrayPos] = sin(time + DTid.x) * 10;
-particles[arrayPos + 1] = abs(sin(DTid.x * 0.0001) * cos(time) * 20);
+particles[arrayPos + 1] = abs(sin(DTid.x * 0.0000001) * cos(time) * 200);
 particles[arrayPos + 2] = cos(time + DTid.x) * 10;
 
 }
