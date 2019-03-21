4eabb1d76c576359035cbb0d0d42cdbe487b6c9b
tofb15 <fas_th@hotmail.com>
2019-03-21 12:00:30 +0100

    Found a good shape

--- assets/D3D12/PixelShader_Particle.hlsl
+++ assets/D3D12/PixelShader_Particle.hlsl
@@ -1,9 +1,10 @@
 struct GSOut
 {
     float4 pos : SV_POSITION;
+	float4 color : COLOR;
 };
 
 float4 main(GSOut input) : SV_TARGET0
 {
-	return float4(1.0f, 1.0f, 1.0f, 1.0f);
+	return input.color;
 }
--- assets/D3D12/VertexShader_Particle.hlsl
+++ assets/D3D12/VertexShader_Particle.hlsl
@@ -3,6 +3,7 @@ RWBuffer<float3> particles : register(u0);
 struct VSOut
 {
 	float4 pos : WPOS;
+	float4 color : COLOR;
 };
 
 VSOut main(uint index : SV_VertexID)
@@ -16,6 +17,7 @@ VSOut main(uint index : SV_VertexID)
 	float z = particles[arr_id+2];
 	
 	output.pos = float4(x, y, z, 1.0f);
+	output.color = float4((index % 1000) / 501, 0.0f , 0.0f, 1.0f);
 	//output.pos = mul(viewPerspective, mul(sr[matrixIndex + instanceID], float4(input.pos, 1.0f)));
 
 	return output;
